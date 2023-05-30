4f9294c42d3a032f3dedb579bf9a873279114b92
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-05-31 01:57:07 +0530

    app recreated | project structured -- components, pages and assets along with react router dom | pages > home > Home.js & Home.css

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='HomeComponent'>
+        <div className="HomeContainer">
+            <h1>ZerOne-Learning | Home Page</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
