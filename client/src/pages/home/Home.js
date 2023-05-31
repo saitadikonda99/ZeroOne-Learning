27ed9c1b7ca3c7785ec472cd21cb9a75eeda18ff
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-05-31 11:08:11 +0530

    test push

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css';
 
+
 const Home = () => {
   return (
     <div className='HomeComponent'>
         <div className="HomeContainer">
             <h1>ZerOne-Learning | Home Page</h1>
+            <h1>Deepak Reddy Gathpa</h1>
         </div>
     </div>
   )
