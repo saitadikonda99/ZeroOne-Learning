bfc617ab06ae7be64a6aeac377e62c5ab8e2d996
sai tadikonda <saiitadikonda7@gmail.com>
2023-05-31 11:23:56 +0530

    sai

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
 
+
 const Home = () => {
   return (
     <div className='HomeComponent'>
         <div className="HomeContainer">
             <h1>ZerOne-Learning | Home Page</h1>
+            <h2>Tadikonda sai manikanta</h2>
+            <h1>Deepak Reddy Gathpa</h1>
         </div>
     </div>
   )
