d33b16284781c53fa61c55e835e3f943a80a20e6
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-05-31 11:20:00 +0530

    branch information added

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Home.css';
 
-
 const Home = () => {
   return (
     <div className='HomeComponent'>
         <div className="HomeContainer">
             <h1>ZerOne-Learning | Home Page</h1>
             <h1>Deepak Reddy Gathpa</h1>
+            <h1>Current Branch - deepak-master</h1>
         </div>
     </div>
   )
