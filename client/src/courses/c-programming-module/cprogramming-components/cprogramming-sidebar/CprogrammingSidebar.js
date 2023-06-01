e200194e1735008514f804180e8e673c5ab2c0b6
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-06-01 19:08:15 +0530

    paths added to content array attributes in c-prog sidebar

--- client/src/courses/c-programming-module/cprogramming-components/cprogramming-sidebar/CprogrammingSidebar.js
+++ client/src/courses/c-programming-module/cprogramming-components/cprogramming-sidebar/CprogrammingSidebar.js
@@ -14,14 +14,17 @@ const routes = [
   {
     name: 'Course Outcome 2',
     content: ['Increment, decrement operators', 'Iterative Statements', 'Loops', 'Nested Loops', 'Bitwise Operators-I', 'Bitwise Operators-II', 'Expression Evaluation Rules', 'Operator Precedency', 'Recursion', 'Problem Solving with Recursion'],
+    paths: ['/courses/c-programming/co1/structured-programming-paradigm', '/courses/c-programming/co1/designing-algorithms', '/courses/c-programming/co1/designing-flowcharts', '/courses/c-programming/co1/primary-data-types', '/courses/c-programming/co1/derived-data-types', '/c-programming/co1/5', '/c-programming/co1/6', '/c-programming/co1/7', '/c-programming/co1/8', '/c-programming/co1/9', '/c-programming/co1/10', '/c-programming/co1/11']
   },
   {
     name: 'Course Outcome 3',
     content: ['Data Types-II', 'Command Line Arguments', 'Strings and String Library', 'Arrays - Creation & Insertion', 'Bubble Sort', 'Linear Search', 'Binary Search', 'Dynamic Memory Allocation' ],
+    paths: ['/courses/c-programming/co1/structured-programming-paradigm', '/courses/c-programming/co1/designing-algorithms', '/courses/c-programming/co1/designing-flowcharts', '/courses/c-programming/co1/primary-data-types', '/courses/c-programming/co1/derived-data-types', '/c-programming/co1/5', '/c-programming/co1/6', '/c-programming/co1/7', '/c-programming/co1/8', '/c-programming/co1/9', '/c-programming/co1/10', '/c-programming/co1/11']
   },
   {
     name: 'Course Outcome 4',
     content: ['2D Arrays - Creation & Insertion', '2D Arrays - Matrixes', 'Matrix Algebra with Arrays', 'Structures', 'Array of Structures', 'Structure Pointers', 'Stacks using Arrays', 'Queues with Arrays', 'Single Linked List (with structures)', 'Delete Operation on Linked List','Non-linear Data Structures'],
+    paths: ['/courses/c-programming/co1/structured-programming-paradigm', '/courses/c-programming/co1/designing-algorithms', '/courses/c-programming/co1/designing-flowcharts', '/courses/c-programming/co1/primary-data-types', '/courses/c-programming/co1/derived-data-types', '/c-programming/co1/5', '/c-programming/co1/6', '/c-programming/co1/7', '/c-programming/co1/8', '/c-programming/co1/9', '/c-programming/co1/10', '/c-programming/co1/11']
   },
 ];
 
