3baa7cb0d2ee285badc7777cab68a05be0e66d1d
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-06-01 14:41:49 +0530

    back to stable version

--- client/src/courses/CourseApp.js
+++ client/src/courses/CourseApp.js
@@ -7,13 +7,18 @@ import {Routes, Route } from 'react-router-dom';
 import CourseNav from './course_components/course_nav/CourseNav';
 import CourseFooter from './course_components/course_footer/CourseFooter';
 
+
+
 const CourseApp = () => {
   return (
     <div className='CourseAppComponent'>
         <div className="CourseAppContainer">
             <CourseNav />
             <div className="CourseAppMain">
+              <h1><h1>Course App file</h1></h1>
+                <Routes>
 
+                </Routes>
             </div>
             <CourseFooter />
         </div>
