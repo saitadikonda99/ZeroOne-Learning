a1fe4e983bbb13f2fdc3c11329cdccadd6057a69
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-06-01 11:33:58 +0530

    hero section added

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -29,6 +29,29 @@ const Home = () => {
 
 
 
+        <div className="one hero">
+          <div className="one-in">
+            <div className="one-in-one">
+                <div className="oio-one">
+                    <div className="oio-one-in">
+                      <p>For Better Future</p>
+                      <h1>Join and become the part of trusted community</h1>
+                      <p id='ptwo'>Find the right instructor for you</p>
+                      <div className="oioo-bi">
+                        <Link className='oioo-bi-links' to='/' >About Us</Link>
+                        <Link className='oioo-bi-links' to='/' >Courses</Link>
+                      </div>
+                    </div>
+                </div>
+                <div className="oio-two">
+
+                </div>
+            </div>
+            <div className="one-in-two">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e74040" fill-opacity="1" d="M0,128L240,160L480,128L720,128L960,64L1200,64L1440,128L1440,0L1200,0L960,0L720,0L480,0L240,0L0,0Z"></path></svg>
+            </div>
+          </div>
+        </div>
 
 
 
@@ -165,14 +188,6 @@ const Home = () => {
                 </div>
             </div>
           </div>
-
-
-
-
-
-
-
-
 
 
           <div className="four">
