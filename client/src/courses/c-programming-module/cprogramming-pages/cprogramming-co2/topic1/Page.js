d5c01f785c6ff64ea6713be943e17df456841993
sai tadikonda <saiitadikonda7@gmail.com>
2023-06-05 11:32:29 +0530

    common css update in co2

--- client/src/courses/c-programming-module/cprogramming-pages/cprogramming-co2/topic1/Page.js
+++ client/src/courses/c-programming-module/cprogramming-pages/cprogramming-co2/topic1/Page.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+
+// imports Here
+import CprogrammingSidebar from '../../../cprogramming-components/cprogramming-sidebar/CprogrammingSidebar';
+import './Page.css'
+import '../../cprogramming-co1/common-css/index.css'
+
 function Page() {
   return (
-    <div>Page</div>
+        <div className="topic-container">
+          <div className="co-sidebar">
+             <CprogrammingSidebar />
+          </div>  
+           <div className="topic-content">
+            <div className="topic-content-head">
+                <h1>Increment, decrement operators</h1>
+                <p>In this page you will learn about Increment, decrement operators
+                   in detail
+                </p>
+            </div>
+          </div>
+      </div>
   )
 }
 
