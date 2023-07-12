68443d180a156ded8374918edbc91b623e624d14
saitadikonda99 <saitadikonda.tech@gmail.com>
2023-07-12 10:13:59 +0530

    basic courses footer added

--- client/src/courses/course_components/course_footer/CourseFooter.js
+++ client/src/courses/course_components/course_footer/CourseFooter.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './CourseFooter.css'
 
+
+// imports start here
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { FaTelegram } from 'react-icons/fa'
+import { BsGithub } from 'react-icons/bs'
+
+
 const CourseFooter = () => {
   return (
     <div className='footer-container'>
-        <h1>Courses Footer Component</h1>
+       <div className="footer-in-one">
+          <h1>ZeroOne Learning</h1>
+       </div>
+        <div className="footer-in-two">
+          <div className="footer-in-names">
+           <p>Designed & Developed By Deepak Reddy Gathpa & Tadikonda Sai Manikanta</p>
+           </div>
+            <div className="footer-in-socials">
+              <AiOutlineInstagram/>
+              <FaTelegram className='telegram'/>
+              <BsGithub/>
+          </div>
+        </div>
     </div>
   )
 }
