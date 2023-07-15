6c39af06b357fd19425c28ce05c47ae13051dfb4
saitadikonda99 <saitadikonda.tech@gmail.com>
2023-07-15 12:09:16 +0530

    course page updated

--- client/src/courses/course_components/course_nav/CourseNav.js
+++ client/src/courses/course_components/course_nav/CourseNav.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import './CourseNav.css'
-import {FaLeftLong} from 'react-icons/fa6'
+import {FiCode} from 'react-icons/fi'
 import {  Link } from 'react-router-dom';
 
 function CourseNav() {
   return (
         <div className="course-nav-container">
-          <h1>NAV BAR </h1>
+          <div className="nav-logo">
+            <div className="nav-logo-head">
+             <FiCode/>
+               </div>
+                 <div className="nav-logo-body">
+                  <h1>zeroOne</h1>
+              </div>
+          </div>
+        <div className="nav-links">
+          <div className="nav-links-in">
+              <Link to='/'>Home</Link>
+              <Link to='/courses'>Courses</Link>
+              <Link to='/contact'>Contact</Link>
+                </div>
+              </div>
+            <div className="nav-button">
+              <button className='nav-button-in'>Join Us</button>
+            </div>
         </div>
   )
 }
--- client/src/courses/course_pages/course_home/CourseHome.js
+++ client/src/courses/course_pages/course_home/CourseHome.js
@@ -16,9 +16,58 @@ const CourseHome = () => {
             </div>
                <div className="CourseHome-Body">
                   <div className="CourseHome-Body-Content">
-                    <h1>Explore Programming's World for Free! </h1>
-                    <h2>with zeroOneLearning! </h2>
+                     <div className="CourseHome-Body-Content-one">
+                        <h1>Explore Programming's World for Free! </h1>
+                           <h2>with zeroOneLearning! </h2>
+                      </div>
+                  <div className="CourseHome-Body-Content-two">
+                  
+                      <div className="courseHome-element-one-content">
+                        <div className="courseHome-element-one-content-in">
+                          <span className='number-css'> 01 </span>
+                        </div>
+                          <div className="courseHome-element-one-content-in-one">
+                              <h1>Theoretical Knowledge</h1>
+                            </div>
+                      </div>
+
+                      <div className="courseHome-element-two-content">
+                      <div className="courseHome-element-two-content-in">
+                      <span className='number-css'> 02 </span>
+                        </div>
+                      <div className="courseHome-element-two-content-in-one">
+                              <h1>Practical Skills</h1>
+                            </div>
+
+                      </div>
+
+                      <div className="courseHome-element-three-content">
+                      <div className="courseHome-element-three-content-in">
+                      <span className='number-css'> 03 </span>
+                        </div>
+                      <div className="courseHome-element-three-content-in-one">
+                              <h1>Work with a Mentor</h1>
+                            </div>
+
+                      </div>
+
+
+                      <div className="courseHome-element-four-content">
+                      <div className="courseHome-element-four-content-in">
+                      <span className='number-css'> 04 </span>
+                        </div>
+                      <div className="courseHome-element-four-content-in-one">
+                              <h1>Final Test or Project</h1>
+                            </div>
+
+                      </div>
+
+
+                       
+
+                  </div>
                 </div>
+
               <div className="course-home-courses">
             <div className="course-home-courses-one">
                   <h1> Start your learning coding </h1> 
