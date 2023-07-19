a3920ccd2af72d71864abf0e8fb0b669912f7e25
saitadikonda99 <saitadikonda.tech@gmail.com>
2023-07-19 14:47:28 +0530

    version 1.0

--- client/src/courses/c-programming-module/cprogramming-pages/CHomePage.js
+++ client/src/courses/c-programming-module/cprogramming-pages/CHomePage.js
@@ -10,8 +10,7 @@ import cimage from '../../../assets/c.png';
 
 // Import other components and modules here
 import Cnavbar from '../C-programming-nav/Cnavbar';
-
-
+import CourseFooter from '../../../components/course_footer/CourseFooter'
  
 
 const CHomePage = () => {
@@ -69,7 +68,7 @@ const CHomePage = () => {
 
 
             <div className="c-prog-footer">
-              
+              <CourseFooter/>
             </div>
 
         </div>
