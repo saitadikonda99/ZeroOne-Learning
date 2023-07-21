fe210a72b17e4ccac20eaa83b112f0cbddeca3f4
saitadikonda99 <saitadikonda.tech@gmail.com>
2023-07-21 15:32:28 +0530

    mobile support removed

--- client/src/LaptopOnlyMessage.js
+++ client/src/LaptopOnlyMessage.js
@@ -2,8 +2,16 @@
 import React from 'react';
 
 const LaptopOnlyMessage = () => (
-  <div style={{ textAlign: 'center', padding: '20px' }}>
-    <h2>This app can only be viewed on laptops</h2>
+  <div style={{ 
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50vh',
+    flexDirection: 'column',
+    fontSize: '1rem',
+   }}>
+    <p>This app can only be viewed on laptops</p>
+    <p> Thank you 😊</p>
   </div>
 );
 
